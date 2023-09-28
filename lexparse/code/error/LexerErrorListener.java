fff33e22d731316d86430f1127c51ba758ec4e41
Dulputer <92662395+ShardulWakade@users.noreply.github.com>
2023-09-27 22:33:42 -0500

    LEXER ERROR -> LEX ERROR

--- lexparse/code/error/LexerErrorListener.java
+++ lexparse/code/error/LexerErrorListener.java
@@ -12,7 +12,7 @@ public class LexerErrorListener extends BaseErrorListener {
     @Override
     public void syntaxError(Recognizer<?, ?> recognizer, Object offendingSymbol, int line, int charPositionInLine,
             String msg, RecognitionException e) {
-        System.out.println("LEXER ERROR");
+        System.out.println("LEX ERROR");
         System.exit(1);
     }
 }
