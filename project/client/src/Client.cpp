eab19c5ca8a6e4425c67bff75e7df70a3201927d
Dulputer <shardulwakade2003@gmail.com>
2023-10-02 23:44:42 -0500

    Improved Client for Checkpoint 1

--- project/client/src/Client.cpp
+++ project/client/src/Client.cpp
@@ -16,7 +16,52 @@ void printFancyExit(){
 }
 
 void printInstructions(){
-    std::cout << "Enter a command and then enter a blank newline to send that command to parser" << std::endl;
+    std::cout << "Enter a command and then type @run to send commands to the lexer/parser" << std::endl;
+    std::cout << "Enter @quit to quit the program." << std::endl;
+}
+
+// true means not EOF. false means EOF
+bool addLinesInto(std::vector<std::string>& lines, std::istream& in){
+    std::string line;
+    while(std::getline(in, line)){
+        if(line.empty()){
+            // Discard and move to next line
+            continue;    
+        }
+        
+        lines.push_back(line); 
+        if(line[0] == '@'){  // This means it is a command, so lets return back to our caller.
+            return true;
+        }
+
+        std::cout << "... ";
+    }
+    return false;
+}
+
+std::vector<LexParseError> runCommand(const std::vector<std::string>& lines){
+    // Append all lines into a single string. Then send to parser
+
+    std::string command;
+    for(const auto& str : lines){
+        command += str;
+        command += '\n';
+    }
+
+    std::cout << "\nSending this command to the parser\n";
+    return checkLexParse(command.c_str());
+}
+
+void printAptErrorMessage(const std::vector<std::string>& lines, const std::vector<LexParseError>& errors){
+    std::cout << "\nThe following errors were found\n" << std::endl;
+    for(const auto& err : errors){
+        std::cout << "Error on line " << err.line << "\n\t" << lines[err.line-1] << "\n\t";
+        
+        std::string spaces(err.charPositionInLine, ' ');
+
+        std::cout << spaces << '^' << std::endl;
+        std::cout << spaces << err.msg << std::endl;
+    }
 }
 
 int main(){
@@ -24,40 +69,33 @@ int main(){
     printInstructions();
 
     std::vector<std::string> lines;
-    std::string line;
     
-    std::cout << "> ";
+    std::cout << ">>> ";
 
-    while(std::getline(std::cin, line)){
-        if(line.empty()){
-            break; // Send to the parser!
-        }
-        
-        lines.push_back(line);
-        std::cout << "...\t";
-    }
+    while(addLinesInto(lines, std::cin)){
+        // If we are here then this means either @run or @quit was called
+        std::string lastCommand = lines.back(); lines.pop_back();
 
-    if(lines.empty()){
-        std::cout << "Nothing to send to the parser!" << std::endl;
-    } 
-    else {
-        std::string command;
-        for(const auto& str : lines){
-            command += str;
-            command += "\n\t";
-        }
-        std::cout << "Sending the following command to the parser : \n" << command << std::endl;
-        
-        std::vector<LexParseError> errors = checkLexParse(command.c_str());
-        if(errors.empty()){
-            std::cout << "No errors!" << std::endl;
-        }
-        else {
-            std::cout << "Errors found : " << std::endl;
-            for(const auto& err : errors){
-                std::cout << err.line << ":" << err.charPositionInLine << "\t\n" << err.msg << std::endl;
+        if(lastCommand == "@quit"){
+            break;
+        } 
+        else if (lastCommand == "@run"){
+            if(lines.empty()){
+                std::cout << "Empty command : nothing to send." << std::endl;
+            }
+            else {
+                std::vector<LexParseError> errors = runCommand(lines);
+                if(errors.empty()){
+                    std::cout << "No errors were found in the message!" << std::endl;
+                } 
+                else {
+                    printAptErrorMessage(lines, errors);
+                }
             }
         }
+
+        lines.clear();
+        std::cout << ">>> ";
     }
 
     printFancyExit();
