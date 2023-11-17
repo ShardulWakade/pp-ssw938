2969493fa3b366df173f83225456fda0467e64ab
Dulputer <shardulwakade2003@gmail.com>
2023-11-16 21:15:02 -0600

    Build process working correctly

--- project/buildAll.sh
+++ project/buildAll.sh
@@ -32,4 +32,9 @@ cd textvisualizer
 ./build.sh
 cd ..
 
+echo "Build successful."
+echo "In order to use any of the programs, cd into its directory and do ./run.sh"
+echo "cd client && ./run.sh"
+echo "cd server && ./run.sh"
+echo "cd textvisualizer && ./run.sh"
 
